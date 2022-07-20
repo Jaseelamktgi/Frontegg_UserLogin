624d11187cafeeb12c8118c12013a13a737eb1de
Jaseela Parveen <jaseelamktgi@gmail.com>
2022-07-20 14:44:30 +0530

    sample userlogin with frontegg

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+ import { FronteggProvider } from '@frontegg/react';
+
+const contextOptions = {
+  baseUrl: 'https://app-8rn3fg5u5ezh.frontegg.com',
+  clientId: '9d8da0ad-a9e0-4b7c-ab85-658a8a16c674'
+};
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>
+  <App />
+  </FronteggProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
